55aa35b10bf6b1079b274638e67a1f003bb9f3f1
neurocurrents <106438899+neurocurrents@users.noreply.github.com>
2025-07-15 09:21:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
